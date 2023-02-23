a7e0a23eb8c016d6b8305ecc145ff2229a261aeb
Aluisio Pereira <109995434+Oaluiser@users.noreply.github.com>
2023-02-23 20:50:44 -0300

    testing trail

---
+++ script.js
@@ -0,0 +1,16 @@
+const mouseTrailer = document.getElementById("mousetrailer");
+const inicio = document.getElementById("inicio");
+const sobremim = document.getElementById("sobremim");
+const portfolio = document.getElementById("portfolio");
+const contato = document.getElementById("contato");
+
+//offsetop
+
+/* addEventListener("mousemove", (event) => {
+    console.log(event.target);
+    const x = event.clientX;
+    const y = event.clientY;
+    
+    mouseTrailer.style.left = `${x}px`;
+    mouseTrailer.style.top = `${y}px`;
+}); */
