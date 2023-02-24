99b5475512a6937fe2d7ae2a1a6dfa605a0d1365
Aluisio Pereira <109995434+Oaluiser@users.noreply.github.com>
2023-02-24 18:55:45 -0300

    mouse trailer implemented

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const mouse = document.getElementById("mouse");
 const mouseTrailer = document.getElementById("mousetrailer");
 const inicio = document.getElementById("inicio");
 const sobremim = document.getElementById("sobremim");
@@ -6,11 +7,20 @@ const contato = document.getElementById("contato");
 
 //offsetop
 
-/* addEventListener("mousemove", (event) => {
+window.onmousemove = event => {
     console.log(event.target);
     const x = event.clientX;
     const y = event.clientY;
     
-    mouseTrailer.style.left = `${x}px`;
-    mouseTrailer.style.top = `${y}px`;
-}); */
+    mouse.style.left = `${x-5}px`;
+    mouse.style.top = `${y-5}px`;
+
+    const keyframes = {
+        transform: `translate(${x-25}px, ${y-25}px)`
+    };
+
+    mousetrailer.animate(keyframes, {
+        duration: 1000,
+        fill: "forwards"
+    });
+}
